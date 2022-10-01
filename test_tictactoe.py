72a1a54dfb54e20aa68259cc375753a4925104db
Michael Morikawa <michael_f_morikawa@csu.fullerton.edu>
2022-09-30 18:36:47 -0700

    Implement minimax and created tests for use with pytest

---
+++ test_tictactoe.py
@@ -0,0 +1,114 @@
+from tictactoe import *
+import pytest
+
+
+class TestActions:
+    def test_empty(self):
+        board = initial_state()
+        exptectedMoves = set([(i, j) for i in range(3) for j in range(3)])
+        assert set(actions(board)) == exptectedMoves
+
+    def test_oneSpot(self):
+        board = [[X, O, X], [O, X, O], [EMPTY, X, O]]
+        assert actions(board) == [(2, 0)]
+
+
+class TestWinner:
+    def test_X_wins_diag(self):
+        board = [[X, O, X], [O, X, O], [EMPTY, EMPTY, X]]
+        board2 = [[EMPTY, O, X], [O, X, EMPTY], [X, EMPTY, EMPTY]]
+        assert winner(board) == X == winner(board2)
+
+    def test_O_wins_diag(self):
+        board = [[O, X, X], [X, O, O], [EMPTY, X, O]]
+        board2 = [[X, X, O], [EMPTY, O, EMPTY], [O, X, EMPTY]]
+        assert winner(board) == O
+        assert winner(board2) == O
+
+    def test_X_wins_row(self):
+        board1 = [[X] * 3, [O, O, EMPTY], [EMPTY] * 3]
+        board2 = [[O, O, EMPTY], [X] * 3, [EMPTY] * 3]
+        board3 = [[O, O, EMPTY], [EMPTY] * 3, [X] * 3]
+        assert winner(board1) == winner(board2) == winner(board3) == X
+
+    def test_O_wins_row(self):
+        board1 = [[O] * 3, [X, X, EMPTY], [EMPTY] * 3]
+        board2 = [[X, X, EMPTY], [O] * 3, [EMPTY] * 3]
+        board3 = [[X, X, EMPTY], [EMPTY] * 3, [O] * 3]
+        assert winner(board1) == winner(board2) == winner(board3) == O
+
+    def test_X_wins_col(self):
+        T = X
+        board1 = [[T, EMPTY, EMPTY]] * 3
+        board2 = [[EMPTY, T, EMPTY]] * 3
+        board3 = [[EMPTY, EMPTY, T]] * 3
+        assert winner(board1) == winner(board2) == winner(board3) == T
+
+    def test_O_wins_col(self):
+        T = O
+        board1 = [[T, EMPTY, EMPTY]] * 3
+        board2 = [[EMPTY, T, EMPTY]] * 3
+        board3 = [[EMPTY, EMPTY, T]] * 3
+        assert winner(board1) == winner(board2) == winner(board3) == T
+    
+    def test_draw(self):
+        board = [[X,O,X],[X,X,O],[O,X,O]]
+        assert winner(board) is None
+
+
+class TestPlayer:
+    def test_initial(self):
+        assert player(initial_state()) == X
+    
+    def test_X(self):
+        board = [[X,O,X], [O,X,O], [EMPTY]*3]
+        assert player(board) == X
+    
+    def test_O(self):
+        board = [[X,O,X], [O,X,O], [X,EMPTY, EMPTY]]
+        assert player(board) == O
+
+class TestResult:
+    def test_invalid_OOB(self):
+        board = initial_state()
+        with pytest.raises(InvalidMove):
+            result(board, (3,1))
+    
+    def test_space_taken(self):
+        board = [[X,EMPTY, EMPTY], [EMPTY]*3, [EMPTY]*3]
+        with pytest.raises(InvalidMove):
+            result(board, (0,0))
+
+    def test_X_move(self):
+        board = initial_state()
+        original_board = deepcopy(board)
+        expected_board = [[X,EMPTY, EMPTY], [EMPTY]*3, [EMPTY]*3]
+        assert result(board,(0,0)) == expected_board
+        assert board == original_board
+    
+    def test_O_move(self):
+        board = [[X,EMPTY, EMPTY], [EMPTY]*3, [EMPTY]*3]
+        original_board = deepcopy(board)
+        expected_board = [[X,O, EMPTY], [EMPTY]*3, [EMPTY]*3]
+        assert result(board,(0,1)) == expected_board
+        assert board == original_board
+
+class TestMinimax:
+    def test_X_win(self):
+        board = [[X,X, EMPTY], [O, O, EMPTY], [EMPTY]*3]
+        assert minimax(board) == (0,2)
+
+    def test_block_O(self):
+        board = [[O,O, EMPTY], [EMPTY, X, EMPTY], [EMPTY, EMPTY, X]]
+        assert minimax(board) == (0,2)
+    
+    def test_O_win(self):
+        board = [ [EMPTY, X, EMPTY], [O,O, EMPTY],[EMPTY, X, X]]
+        assert minimax(board) == (1,2)
+
+    def test_block_X(self):
+        board = [[X,O, EMPTY], [O, X, EMPTY], [EMPTY, X, EMPTY]]
+        assert minimax(board) == (2,2)
+
+
+
--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,13 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy
+
+
+class InvalidMove(Exception):
+    "Custom exception to be raised in the results() when an invalid move is passed."
+    pass
+
 
 X = "X"
 O = "O"
@@ -13,9 +20,8 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
+
 
 # Dylan M
 def player(board):
@@ -24,12 +30,14 @@ def player(board):
     """
     raise NotImplementedError
 
+
 # Dylan M
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
     raise NotImplementedError
+
 
 # Kevin
 def result(board, action):
@@ -38,12 +46,14 @@ def result(board, action):
     """
     raise NotImplementedError
 
-# Kevin 
+
+# Kevin
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
     raise NotImplementedError
+
 
 # Dylan T
 def terminal(board):
@@ -52,6 +62,7 @@ def terminal(board):
     """
     raise NotImplementedError
 
+
 # Dylan T
 def score(board):
     """
@@ -59,9 +70,46 @@ def score(board):
     """
     raise NotImplementedError
 
+
+def minvalue(board):
+    if terminal(board):
+        return score(board)
+    v = math.inf
+    for action in actions(board):
+        v = min(v, maxvalue(result(board, action)))
+    return v
+
+
+def maxvalue(board):
+    if terminal(board):
+        return score(board)
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, minvalue(result(board, action)))
+    return v
+
+
 # Michael M
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    optimal_action = None
+    if player(board) == X:
+        best_val = -math.inf
+        for action in actions(board):
+            value = minvalue(result(board, action))
+            if value > best_val:
+                best_val = value
+                optimal_action = action
+    elif player(board) == O:
+        best_val = math.inf
+        for action in actions(board):
+            value = maxvalue(result(board, action))
+            if value < best_val:
+                best_val = value
+                optimal_action = action
+    return optimal_action
